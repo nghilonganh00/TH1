91a1de7905c670fff15145ab3878dae2d0c6c0b9
nghilonganh00 <87319148+nghilonganh00@users.noreply.github.com>
2024-03-04 11:10:49 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,455 @@
+const exam = {
+    'part1': {
+        1: {
+            'topic': "Thiên nhiên Việt Nam khác hẳn với thiên nhiên các nước có cùng vĩ độ Tây Á, Đông Phi và Tây Phi là do Việt Nam nằm trong khu vực nhiệt đới gió mùa",
+            'correctAnswer': '1',
+        },
+        2: {
+            'topic': " Xu hướng quốc tế hóa và khu vực hóa nền kinh tế thế giới diễn ra với quy mô lớn và nhịp dộ cao là điều kiện để nước ta tận dụng các nguồn lực bên ngoài để phát triển kinh tế xã hội",
+            'correctAnswer': ''
+        },
+        3: {
+            'topic': "Những trở ngại chính đối với việc phát triển kinh tế xã hội ở nước ta về TNTN là: Trữ lượng ít",
+            'correctAnswer': ''
+        },
+        
+        4: {
+            'topic': "Tài nguyên giữ vị trí quan trọng nhất Việt Nam hiện nay là: Tài nguyên đất",
+            'correctAnswer': ''
+        },
+        5: {
+            'topic': "Tài nguyên có ý nghĩa đặc biệt dối với sự phát triển kinh tế xã hội Việt Nam hiện nay là: Tài nguyên đất",
+            'correctAnswer': ''
+        }, 
+        6: {
+            'topic': "Trong các tài nguyên sau loại nào bị suy giảm nghiêm trọng nhất: Tài nguyên đất",
+            'correctAnswer': ''
+        },
+        7: {
+            'topic': "Để phát triển kinh tế của đất nước cần phải: Để phát triển kinh tế của đất nước cần phải:",
+            'correctAnswer': ''
+        },
+        8: {
+            'topic': "Để phát triển kinh tế của đất nước cần phải: Cấu trúc địa chất ",
+            'correctAnswer': ''
+        }, 
+        9: {
+            'topic': "Tài nguyên đất của Việt Nam rất phong phú, trong đó có nhiều nhất là; Đất đồng cỏ",
+            'correctAnswer': ''
+        },
+        10: {
+            'topic': "Sự khác nhau cơ bản giữa đất phù sa đồng bằng sông Hồng và đất phù sa đồng bằng sông Cửu Long là:",
+            'correctAnswer': 'A. Sự màu mỡ'
+        }
+    }, 
+    'part2': {
+        11: {
+            'topic': "Đặc diểm của đất feralit là",
+            'answerA': "Thường có màu đỏ, phèn, chau, nghèo, mùn",
+            'answerB': "Thường có màu đen, xốp thoát nước",
+            'answerC': "Thường có màu đỏ, vàng rất màu mỡ",
+            'answerD': "Thường có màu nâu, khô không thích hợp trồng lúa",
+            'correctAnswer': "A"
+        },
+        12: {
+            'topic': "Nguồn lực phát triển kinh tế xã hội là:",
+            'answerA': "Toàn bộ các điều kiện tự nhiên, tài nguyên thiên nhiên, cơ sở vật chất, con người, đường lối chính sách ảnh hưởng đến sự phát triển của nền kinh tế xã hội",
+            'answerB': "Những nguồn tài nguyên đa dạng, phong phú của đất nước",
+            'answerC': "Điều kiện thường xuyên và cần thiết để phát triển xã hội loài người",
+            'answerD': "Tất cả những gì bao quanh con người",
+            'correctAnswer': "A"
+        },
+        13: {
+            'topic': "Nhiệt độ trung bình hằng năm của Việt Nam là:",
+            'answerA': "20 độ C",
+            'answerB': "20 độ C",
+            'answerC': "22-27 độ C",
+            'answerD': "22-27 độ C",
+            'correctAnswer': "A"
+        },
+        14: {
+            'topic': "Thuận lợi nhất của khí hậu nước ta đối với sự phát triển kinh tế là",
+            'answerA': "Giao thông vận tải hoạt động thuận lợi quanh năm",
+            'answerB': "Phát triển một nền nông nghiệp đa dạng phong phú",
+            'answerC': "Đáp ứng tốt việc tưới tiêu cho nông nghiệp",
+            'answerD': "Đáp ứng tốt việc tưới tiêu cho nông nghiệp",
+            'correctAnswer': "A"
+        },15: {
+            'topic': "Tài nguyên khoáng sản Việt Nam tập trung nhiều nhất ở:",
+            'answerA': "Miền Bắc",
+            'answerB': "Miền Trung",
+            'answerC': "Miền Nam",
+            'answerD': "Miền đồng bằng",
+            'correctAnswer': "A"
+        },
+        16: {
+            'topic': "Diện tích rừng ở Việt Nam năm 1990 là:",
+            'answerA': "14 triệu ha",
+            'answerB': "10 triệu ha",
+            'answerC': "9 triệu ha",
+            'answerD': "9,5 triệu ha",
+            'correctAnswer': "A"
+        },
+        17: {
+            'topic': "Diện tích rừng ở Việt Nam năm 1990 là:",
+            'answerA': "14 triệu ha",
+            'answerB': "10 triệu ha",
+            'answerC': "9 triệu ha",
+            'answerD': "9,5 triệu ha",
+            'correctAnswer': "A"
+        },
+        18: {
+            'topic': "Nhịp độ gia tăng dân số biến đổi qua các thời kỳ, và tăng nhanh nhất qua các kỳ:",
+            'answerA': "1931- 1960",
+            'answerB': "1965- 1975",
+            'answerC': "1979- 1989",
+            'answerD': "1990- 2000",
+            'correctAnswer': "A"
+        },
+        19: {
+            'topic': "Gia tăng dân số tự nhiên là:",
+            'answerA': "Hiệu số của người nhập cư và người xuất cư",
+            'answerB': "Hiệu số của tỉ suất sinh và tỉ suất tử",
+            'answerC': "Hiệu số của tỉ suất sinh và tỉ suất tử",
+            'answerD': "Tuổi thọ trung bình cao",
+            'correctAnswer': "A"
+        },
+        20: {
+                'topic': "Khu vực có gia tăng dân số tự nhiên cao nhất nước ta là:",
+                'answerA': "Đồng Bằng Sông Hậu",
+                'answerB': "Đồng bằng Sông Cửu Long",
+                'answerC': "Tây Nguyên",
+                'answerD': "Trung Du và Miền núi phía Bắc",
+                'correctAnswer': "A"
+        }
+
+    }, 
+    'part3': {
+        21: {
+            'topic': "Nguyên nhân dẫn đến sự bùng nổ dân số là",
+            'answerA': "tỷ lệ sinh cao",
+            'answerB': "số người nhập cư nhiều",
+            'answerC': "dân số tăng quá nhanh",
+            'answerD': "tuổi thọ trung bình cao",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        22: {
+            'topic': "Giải pháp nào hợp lý nhất để tạo sự cân đối dân cư",
+            'answerA': "Giảm tỉ lệ sinh ở những vùng đông dân",
+            'answerB': "Di cư từ dồng bằng lên miền núi",
+            'answerC': "Tiến hành đô thị hóa nông thôn",
+            'answerD': "Phân bố lại dân cư ở các vùng, ngành và các miền",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        23: {
+            'topic': "Các tỉnh, thành phố có tỉ lệ thất nghiệp cao nhất là:",
+            'answerA': "Hà Nội, Hải Phòng, Thái Nguyên",
+            'answerB': "Hà Nội, Thái Bình, Hưng Yên.",
+            'answerC': "Hà Nội, Thái Bình, Hải Phòng, Hà Tây.",
+            'answerD': "Thái Bình, Thanh Hóa.",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        24: {
+            'topic': "Nơi có tỉ lệ thiếu việc làm cao nhất ở Việt Nam là",
+            'answerA': "Tây Nguyên",
+            'answerB': "Đồng Bằng Sông Hậu",
+            'answerC': "Đồng Bằng Sông Cửu Long",
+            'answerD': "Đồng bằng duyên hải miền Trung",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        25: {
+            'topic': "Chất lượng cuộc sống là:",
+            'answerA': "Khái niệm phản ánh độ được đáp ứng những nhu cầu cơ bản của con người về vật chất, tinh thần và chất lương môi trường.",
+            'answerB': "Sự phản ánh tuổi thọ trung bình của dân cư",
+            'answerC': "Sự phản ánh mức độ học vấn của người dân",
+            'answerD': " Sự phản ánh mức độ sống của người dân",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        26: {
+            'topic': "Nơi thu nhập bình quân đầu người cao nhất nước ta là:",
+            'answerA': "tỷ lệ sinh cao",
+            'answerB': "số người nhập cư nhiều",
+            'answerC': "dân số tăng quá nhanh",
+            'answerD': "tuổi thọ trung bình cao",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        27: {
+            'topic': "Để đảm bảo công bằng xã hội, văn minh, đảm bảo sự phát triển các nguồn lực con người và phát triển bền vững về kinh tế xã hội thì vấn đề cấp thiết đặt ra với nước ta hiện nay là:",
+            'answerA': "tỷ lệ sinh cao",
+            'answerB': "số người nhập cư nhiều",
+            'answerC': "dân số tăng quá nhanh",
+            'answerD': "tuổi thọ trung bình cao",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        28: {
+            'topic': "Phương hướng xóa đói giảm nghèo ở nước ta hiện nay là:",
+            'answerA': "tỷ lệ sinh cao",
+            'answerB': "số người nhập cư nhiều",
+            'answerC': "dân số tăng quá nhanh",
+            'answerD': "tuổi thọ trung bình cao",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        29: {
+            'topic': "Lực lượng lao động có kỹ thuật tập trung đông nhất ở:",
+            'answerA': "tỷ lệ sinh cao",
+            'answerB': "số người nhập cư nhiều",
+            'answerC': "dân số tăng quá nhanh",
+            'answerD': "tuổi thọ trung bình cao",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        30: {
+            'topic': "Nguyên nhân làm cho người lao động nước ta có thu nhập thấp là do:",
+            'answerA': "tỷ lệ sinh cao",
+            'answerB': "số người nhập cư nhiều",
+            'answerC': "dân số tăng quá nhanh",
+            'answerD': "tuổi thọ trung bình cao",
+            'correctAnswer': ['A', "B", "C"]
+        },
+
+    },
+    'part4': {
+        31: {
+            'topic': "Chiếm tỉ lệ lao động cao nhất hiện nay là",
+            'correctAnswer': "9"
+        },
+        32: {
+            'topic': "Để nâng cao chất lượng về mặt văn hóa trong đời sống văn hóa - xã hội thì cần phải.",
+            'correctAnswer': "9"
+        },
+        33: {
+            'topic': " Tỷ lệ tử vong trẻ sơ sinh giảm, tỷ lệ tuổi thọ trung bình của người dân tăng là do:",
+            'correctAnswer': "9"
+        },
+        34: {
+            'topic': "Nguyên nhân chính làm cho nước ta có điều kiện tiếp xúc với nền văn hóa thế giới là do:",
+            'correctAnswer': "9"
+        },
+        35: {
+            'topic': "Điểm xuất phát của nền kinh tế nước ta:",
+            'correctAnswer': "9"
+        },
+        36: {
+            'topic': "Điểm xuất phát của nền kinh tế nước ta:",
+            'correctAnswer': "9"
+        },
+        37: {
+            'topic': "Vùng chuyên môn hóa về lương thực thực phẩm lớn nhất nước ta là",
+            'correctAnswer': "9"
+        },
+        38: {
+            'topic': "Vùng chuyên môn hóa về lương thực thực phẩm lớn nhất nước ta là",
+            'correctAnswer': "9"
+        },
+        39: {
+            'topic': "Vùng chuyên môn hóa về lương thực thực phẩm lớn nhất nước ta là",
+            'correctAnswer': "9"
+        },
+        40: {
+            'topic': "Vùng chuyên môn hóa về lương thực thực phẩm lớn nhất nước ta là",
+            'correctAnswer': "9"
+        }
+
+    }
+}
+
+const userAnswer = {}
+
+const redirectPage = (newPage) => {
+    window.location.href = newPage
+}
+
+const result = {
+    1: '1', 
+    2: '1',
+    3: '1',
+    4: '1',
+    5: '1',
+    7: '1', 
+    8: '1',
+    9: '1',
+    10: '1',
+    12: '1',
+    13: '1', 
+    14: '1',
+    15: '1',
+    16: '1',
+    17: '1',
+    18: '1',
+    19: '1',
+    20: '1',
+    21: '1',
+    22: '1',
+    23: '1', 
+    24: '1',
+    25: '1',
+    26: '1',
+    27: '1',
+    28: '1',
+    29: '1',
+    30: '1',
+    31: '1',
+    32: '1',
+    33: '1',
+    34: '1',
+    20: '1',
+    21: '1',
+    22: '1',
+    23: '1', 
+    24: '1',
+    25: '1',
+    26: '1',
+    27: '1',
+    28: '1',
+    29: '1',
+    30: '1',
+    31: '1',
+    32: '1',
+    33: '1',
+    34: '1',
+    35: '1',
+    36: '1',
+    37: '1',
+    38: '1',
+    39: '1',
+    40: '1',
+}
+
+const loadExamPage = () => {
+    const {part1, part2, part3, part4} = exam
+    const examPart1 = document.getElementById("exam-part-1")
+    const examPart2 = document.getElementById("exam-part-2")
+    const examPart3 = document.getElementById('exam-part-3')
+    const examPart4 = document.getElementById("exam-part-4")
+
+    Object.keys(part1).forEach((questionKey) => {
+        const question = part1[questionKey]
+        console.log(part1)
+        const {topic, correctAnswer} = question
+        const questionBox = 
+            `<div class="part-1-question">
+                <div class="topic part-1-topic">Câu ${questionKey}: ${topic}</div>
+                <div class="part-1-answers">
+                    <div class="part-1-answer">
+                        <input value="1" type="radio" name="part-1-answer-${questionKey}" onclick="onChangeAnswerPart2(${questionKey}, '1')">
+                        Đúng
+                    </div>
+                    <div class="part-1-answer">
+                        <input value="0" type="radio" name="part-1-answer-${questionKey}" onclick="onChangeAnswerPart2(${questionKey}, '0')">
+                        Sai
+                    </div>
+                </div>
+            </div>`
+        examPart1.innerHTML += questionBox
+
+        userAnswer[questionKey] = ''
+    })
+
+    Object.keys(part2).forEach((questionKey) => {
+        const question = part2[questionKey]
+        const {topic, answerA, answerB, answerC, answerD, correctAnswer} = question
+        const questionBox = 
+            `<div class="part-1-question">
+                <div class="part-1-topic">Câu ${questionKey}: ${topic}</div>
+                <div class="part-1-answers">
+                    <div class="part-1-answer">
+                        <input type="radio" name="part-2-answer-${questionKey} value="A" onclick="onChangeAnswerPart2(${questionKey}, 'A')">
+                        A. ${answerA}.
+                    </div>
+                    <div class="part-1-answer">
+                        <input type="radio" name="part-2-answer-${questionKey} value="B"  onclick="onChangeAnswerPart2(${questionKey}, 'B')">
+                        B.  ${answerB}
+                    </div>
+                    <div class="part-1-answer">
+                        <input type="radio" name="part-2-answer-${questionKey} value="C"  onclick="onChangeAnswerPart2(${questionKey}, 'C')">
+                        C.  ${answerC}
+                    </div>
+                    <div class="part-1-answer">
+                        <input type="radio" name="part-2-answer-${questionKey} value="D"  onclick="onChangeAnswerPart2(${questionKey}, 'D')">
+                        D. ${answerD}.
+                    </div>
+                </div>
+            </div>`
+        examPart2.innerHTML += questionBox
+
+        userAnswer[questionKey] = ''
+    })
+
+    Object.keys(part3).forEach((questionKey) => {
+        const question = part3[questionKey]
+        const {topic, answerA, answerB, answerC, answerD, correctAnswer} = question
+        const questionBox = 
+            `<div class="part-1-question">
+            <div class="part-1-topic">Câu ${questionKey}: ${topic}</div>
+            <div class="part-1-answers">
+                <div class="part-1-answer">
+                    <input type="checkbox" onclick="onChangeAnswerPart3(${questionKey} , 'A')">
+                    A. ${answerA}
+                </div>
+                <div class="part-1-answer" onclick="onChangeAnswerPart3(${questionKey}, 'B')">
+                    <input type="checkbox">
+                    B. ${answerB}
+                </div>
+                <div class="part-1-answer">
+                    <input type="checkbox" onclick="onChangeAnswerPart3(${questionKey} ,'C')">
+                    C. ${answerC}
+                </div>
+                <div class="part-1-answer" onclick="onChangeAnswerPart3(${questionKey}, 'D')">
+                    <input type="checkbox">
+                    D. ${answerD}
+                </div>
+            </div>
+        </div>`
+        examPart3.innerHTML += questionBox
+
+        userAnswer[questionKey] = []
+    })
+
+    Object.keys(part4).forEach((questionKey) => {
+        const question = part4[questionKey]
+        const {topic, answerA, answerB, answerC, answerD, correctAnswer} = question
+        const questionBox = 
+            `<div class="part-1-question">
+                <div class="part-1-topic">Câu ${questionKey}: ${topic}</div>
+                <div class="part-1-answers">
+                    <div class="part-1-answer">
+                        <input type="text" name="part-1-answer" onchange="onChangeAnswerPart4(${questionKey})" id="question-${questionKey}">
+                    </div>
+                    
+                </div>
+            </div>`
+        examPart4.innerHTML += questionBox
+
+        userAnswer[questionKey] = ''
+    })
+
+    console.log(userAnswer)
+}
+
+const loadResultPage = () => {
+    let correctAnswer = 0
+    for(let i = 1; i <= 40; i++) {
+        if(userAnswer[i] === result[i]) {
+            correctAnswer += 1
+        }
+    }
+    document.getElementById('result-scores').innerText = `${correctAnswer}/40`
+}
+
+const onChangeAnswerPart2 = (questionKey, answer) => {
+    userAnswer[questionKey] = answer
+    console.log(userAnswer)
+}
+
+const onChangeAnswerPart3 = (questionKey, answer) => {
+    userAnswer[questionKey].includes(answer) ? userAnswer[questionKey].filter((item) => item === answer) : userAnswer[questionKey].push(answer)
+    console.log(userAnswer)
+}
+
+
+
+const onChangeAnswerPart4 = (questionKey) => {
+    const question_id = `question-${questionKey}`
+    userAnswer[questionKey] = document.getElementById(question_id).value
+    console.log(userAnswer)
+}
+
